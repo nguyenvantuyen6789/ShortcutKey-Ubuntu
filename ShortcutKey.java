d26740204431104f630f41bc975ac531ea1cbb34
Nguyễn Văn Tuyên <v.tuyennv18@vinhomes.vn>
2022-03-08 14:16:37 +0700

    Update

--- ShortcutKey.java
+++ ShortcutKey.java
@@ -1,4 +1,3 @@
-package com.tuyennguyen;
 
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -21,50 +20,61 @@ public class ShortcutKey extends javax.swing.JFrame {
      * text 04 String
      */
     private final String TEXT_04 = "N8Wn5GENrtHjGXiTKYj7";
+    /**
+     * text 05 String
+     */
+    private final String TEXT_05 = "5555";
 
     public ShortcutKey() {
         initComponents();
         setAlwaysOnTop(true);
         setLocation(1900, 200);
         setResizable(false);
-        
+
         // set text
         jTextField1.setText(TEXT_01);
         jTextField2.setText(TEXT_02);
         jTextField3.setText(TEXT_03);
         jTextField4.setText(TEXT_04);
+        jTextField5.setText(TEXT_05);
+
         // set position
         jTextField1.setCaretPosition(0);
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Ubuntu", 0, 9)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Ubuntu", 0, 9));
         jLabel1.setText("01");
 
-        jLabel2.setFont(new java.awt.Font("Ubuntu", 0, 9)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Ubuntu", 0, 9));
         jLabel2.setText("02");
 
-        jLabel3.setFont(new java.awt.Font("Ubuntu", 0, 9)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Ubuntu", 0, 9));
         jLabel3.setText("03");
 
-        jLabel4.setFont(new java.awt.Font("Ubuntu", 0, 9)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Ubuntu", 0, 9));
         jLabel4.setText("04");
 
-        jTextField1.setFont(new java.awt.Font("Ubuntu", 0, 12)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Ubuntu", 0, 9));
+        jLabel5.setText("05");
+
+        jTextField1.setFont(new java.awt.Font("Ubuntu", 0, 12));
         jTextField1.setText("jTextField1");
         jTextField1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -72,7 +82,7 @@ public class ShortcutKey extends javax.swing.JFrame {
             }
         });
 
-        jTextField2.setFont(new java.awt.Font("Ubuntu", 0, 12)); // NOI18N
+        jTextField2.setFont(new java.awt.Font("Ubuntu", 0, 12));
         jTextField2.setText("jTextField1");
         jTextField2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -80,7 +90,7 @@ public class ShortcutKey extends javax.swing.JFrame {
             }
         });
 
-        jTextField3.setFont(new java.awt.Font("Ubuntu", 0, 12)); // NOI18N
+        jTextField3.setFont(new java.awt.Font("Ubuntu", 0, 12));
         jTextField3.setText("jTextField1");
         jTextField3.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -96,6 +106,14 @@ public class ShortcutKey extends javax.swing.JFrame {
             }
         });
 
+        jTextField5.setFont(new java.awt.Font("Ubuntu", 0, 12)); // NOI18N
+        jTextField5.setText("jTextField1");
+        jTextField5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTextField5MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -106,10 +124,14 @@ public class ShortcutKey extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jLabel3)
                     .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+
                     .addComponent(jLabel2))
                 .addContainerGap(16, Short.MAX_VALUE))
         );
@@ -131,33 +153,44 @@ public class ShortcutKey extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+
+            )
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
     public void copy(String text) {
         Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
         Clipboard clipboard = defaultToolkit.getSystemClipboard();
         clipboard.setContents(new StringSelection(text), null);
     }
-    
-    private void jTextField1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField1MouseClicked
+
+    private void jTextField1MouseClicked(java.awt.event.MouseEvent evt) {
         copy(jTextField1.getText());
-    }//GEN-LAST:event_jTextField1MouseClicked
+    }
 
-    private void jTextField2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField2MouseClicked
+    private void jTextField2MouseClicked(java.awt.event.MouseEvent evt) {
         copy(jTextField2.getText());
-    }//GEN-LAST:event_jTextField2MouseClicked
+    }
 
-    private void jTextField3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField3MouseClicked
+    private void jTextField3MouseClicked(java.awt.event.MouseEvent evt) {
         copy(jTextField3.getText());
-    }//GEN-LAST:event_jTextField3MouseClicked
+    }
 
-    private void jTextField4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField4MouseClicked
+    private void jTextField4MouseClicked(java.awt.event.MouseEvent evt) {
         copy(jTextField4.getText());
-    }//GEN-LAST:event_jTextField4MouseClicked
+    }
+
+    private void jTextField5MouseClicked(java.awt.event.MouseEvent evt) {
+        copy(jTextField5.getText());
+    }
 
     public static void main(String args[]) {
         try {
@@ -189,9 +222,12 @@ public class ShortcutKey extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
-    // End of variables declaration//GEN-END:variables
+    private javax.swing.JTextField jTextField5;
+
 }
